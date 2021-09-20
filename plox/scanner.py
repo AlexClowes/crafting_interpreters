af7149f5b8384c1c75c8b8b220b07ac4fc65e8f0
Alex Clowes <alexclowes@gmail.com>
2021-09-20 22:09:38 +0100

    4.6 Longer Lexemes

--- plox/scanner.py
+++ plox/scanner.py
@@ -52,14 +52,66 @@ class Scanner:
         elif c == ">":
             token = TokenType.GREATER_EQUAL if self.match("=") else TokenType.GREATER
             self.add_token(token)
+        elif c == "/":
+            if self.match("/"):
+                # A comment goes until the end of the line
+                while self.peek() != "\n" and not self.is_at_end():
+                    self.advance()
+            else:
+                self.add_token(TokenType.SLASH)
+        elif c in " \r\t":
+            pass
+        elif c == "\n":
+            self.line += 1
+        elif c == '"':
+            self.string()
+        elif c.isdigit():
+            self.number()
         else:
             lox.error(self.line, "Unexpected character")
+
+    def string(self):
+        while self.peek() != '"' and not self.is_at_end():
+            if self.peek() == "\n":
+                self.line += 1
+            self.advance()
+
+        if self.is_at_end():
+            lox.error(self.line, "Unterminated string")
+            return
+
+        self.advance()  # The closing "
+
+        # Trim the surrounding quotes
+        value = self.source[self.start + 1 : self.current - 1]
+        self.add_token(TokenType.STRING, value)
+
+    def number(self):
+        while self.peek().isdigit():
+            self.advance()
+        # Look for a fractional part
+        if self.peek() == "." and self.peek_next().isdigit():
+            # Consume the .
+            self.advance()
+            while self.peek().isdigit():
+                self.advance()
+        self.add_token(TokenType.NUMBER, float(self.source[self.start : self.current]))
 
     def match(self, expected):
         if self.is_at_end() or self.source[self.current] != expected:
             return False
         self.current += 1
         return True
+
+    def peek(self):
+        if self.is_at_end():
+            return "\0"
+        return self.source[self.current]
+
+    def peek_next(self):
+        if self.current + 1 >= len(self.source):
+            return "\0"
+        return self.source[self.current + 1]
 
     def is_at_end(self):
         return self.current >= len(self.source)
