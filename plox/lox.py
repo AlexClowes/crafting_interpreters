78145014a7e7d7a6f40cddcdf0367b9140b455e7
Alex Clowes <alexclowes@gmail.com>
2021-09-20 21:51:50 +0100

    Replace Lox class with bare functions

--- plox/lox.py
+++ plox/lox.py
@@ -1,47 +1,54 @@
 import sys
 
+import scanner
 
-class Lox:
-    def __init__(self):
-        self.had_error = False
 
-    def error(self, line, message):
-        self.report(line, "", message)
+HAD_ERROR = False
 
-    def report(self, line, where, message):
-        print(f"[line {line}] Error{where}: {message}")
-        self.had_error = True
 
-    @staticmethod
-    def run(source):
-        for token in Scanner(source).scan_tokens():
-            print(token)
+def error(line, message):
+    report(line, "", message)
 
-    def run_file(self, path):
-        with open(path) as f:
-            self.run(f.read())
-        if self.had_error:
-            sys.exit(65)
 
-    def run_prompt(self):
-        while True:
-            try:
-                line = input("> ")
-            except EOFError:
-                break
-            self.run(line)
-            self.had_error = False
+def report(line, where, message):
+    print(f"[line {line}] Error{where}: {message}")
+    global HAD_ERROR
+    HAD_ERROR = True
 
-    def main(self):
-        args = sys.argv[1:]
-        if len(args) > 1:
-            print("Usage: plox [script]")
-            sys.exit(64)
-        elif len(args) == 1:
-            self.run_file(args[0])
-        else:
-            self.run_prompt()
+
+def run(source):
+    for token in scanner.Scanner(source).scan_tokens():
+        print(token)
+
+
+def run_file(path):
+    with open(path) as f:
+        run(f.read())
+    if HAD_ERROR:
+        sys.exit(65)
+
+
+def run_prompt():
+    while True:
+        try:
+            line = input("> ")
+        except EOFError:
+            break
+        run(line)
+        global HAD_ERROR
+        HAD_ERROR = False
+
+
+def main():
+    args = sys.argv[1:]
+    if len(args) > 1:
+        print("Usage: plox [script]")
+        sys.exit(64)
+    elif len(args) == 1:
+        run_file(args[0])
+    else:
+        run_prompt()
 
 
 if __name__ == "__main__":
-    Lox().main()
+    main()
