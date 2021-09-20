03fef47f814b6803e91aeef66b77e35747e5c7c8
Alex Clowes <alexclowes@gmail.com>
2021-09-20 21:52:04 +0100

    4.5 Recognising lexemes

--- plox/scanner.py
+++ plox/scanner.py
@@ -1,3 +1,4 @@
+import lox
 from tokens import Token, TokenType
 
 
@@ -17,5 +18,58 @@ class Scanner:
         self.tokens.append(Token(TokenType.EOF, "", None, self.line))
         return self.tokens
 
+    def scan_token(self):
+        c = self.advance()
+        if c == "(":
+            self.add_token(TokenType.LEFT_PAREN)
+        elif c == ")":
+            self.add_token(TokenType.RIGHT_PAREN)
+        elif c == "{":
+            self.add_token(TokenType.LEFT_BRACE)
+        elif c == "}":
+            self.add_token(TokenType.RIGHT_BRACE)
+        elif c == ",":
+            self.add_token(TokenType.COMMA)
+        elif c == ".":
+            self.add_token(TokenType.DOT)
+        elif c == "-":
+            self.add_token(TokenType.MINUS)
+        elif c == "+":
+            self.add_token(TokenType.PLUS)
+        elif c == ";":
+            self.add_token(TokenType.SEMICOLON)
+        elif c == "*":
+            self.add_token(TokenType.STAR)
+        elif c == "!":
+            token = TokenType.BANG_EQUAL if self.match("=") else TokenType.BANG
+            self.add_token(token)
+        elif c == "=":
+            token = TokenType.EQUAL_EQUAL if self.match("=") else TokenType.EQUAL
+            self.add_token(token)
+        elif c == "<":
+            token = TokenType.LESS_EQUAL if self.match("=") else TokenType.LESS
+            self.add_token(token)
+        elif c == ">":
+            token = TokenType.GREATER_EQUAL if self.match("=") else TokenType.GREATER
+            self.add_token(token)
+        else:
+            lox.error(self.line, "Unexpected character")
+
+    def match(self, expected):
+        if self.is_at_end() or self.source[self.current] != expected:
+            return False
+        self.current += 1
+        return True
+
     def is_at_end(self):
         return self.current >= len(self.source)
+
+    def advance(self):
+        ret = self.source[self.current]
+        self.current += 1
+        return ret
+
+    def add_token(self, type, literal=None):
+        self.tokens.append(
+            Token(type, self.source[self.start : self.current], literal, self.line)
+        )
