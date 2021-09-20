9994ff5198051793853b6bb9ceb377b4998017d7
Alex Clowes <alexclowes@gmail.com>
2021-09-20 21:26:52 +0100

    Combine token.py and token_type.py into tokens.py

--- plox/tokens.py
+++ plox/tokens.py
@@ -50,3 +50,14 @@ TokenType = Enum(
         "EOF",
     ],
 )
+
+
+class Token:
+    def __init__(self, type, lexeme, literal, line):
+        self.type = type
+        self.lexeme = lexeme
+        self.literal = literal
+        self.line = line
+
+    def __str__(self):
+        return f"{self.type} {self.lexeme} {self.literal}"
