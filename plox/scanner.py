33e44cdd0f56641b60b3c2492d945e752d5b36f9
Alex Clowes <alexclowes@gmail.com>
2021-09-20 21:27:11 +0100

    4.4 The Scanner Class

---
+++ plox/scanner.py
@@ -0,0 +1,21 @@
+from tokens import Token, TokenType
+
+
+class Scanner:
+    def __init__(self, source):
+        self.source = source
+        self.tokens = []
+        self.start = 0
+        self.current = 0
+        self.line = 1
+
+    def scan_tokens(self):
+        while not self.is_at_end():
+            # We are at the beginning of the next lexeme
+            self.start = self.current
+            self.scan_token()
+        self.tokens.append(Token(TokenType.EOF, "", None, self.line))
+        return self.tokens
+
+    def is_at_end(self):
+        return self.current >= len(self.source)
