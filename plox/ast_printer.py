7aaa7c7b3b621b101f71dbb6eb99c6e6921f7bfd
Alex Clowes <alexclowes@gmail.com>
2021-09-24 00:11:16 +0100

    5.4 A (Not Very) Pretty Printer

---
+++ plox/ast_printer.py
@@ -0,0 +1,35 @@
+import expr
+from tokens import Token, TokenType
+
+
+class ASTPrinter:
+    def print(self, expr):
+        return expr.accept(self)
+
+    def parenthesize(self, name, *exprs):
+        return "(" + name + " " + " ".join(map(self.print, exprs)) + ")"
+
+    def visit_binary(self, expr):
+        return self.parenthesize(expr.operator.lexeme, expr.left, expr.right)
+
+    def visit_grouping(self, expr):
+        return self.parenthesize("group", expr.expression)
+
+    def visit_literal(self, expr):
+        return str(expr.value) if expr.value else "nil"
+
+    def visit_unary(self, expr):
+        return self.parenthesize(expr.operator.lexeme, expr.right)
+
+
+def main():
+    expression = expr.Binary(
+        expr.Unary(Token(TokenType.MINUS, "-", None, 1), expr.Literal(123)),
+        Token(TokenType.STAR, "*", None, 1),
+        expr.Grouping(expr.Literal(45.67)),
+    )
+    print(ASTPrinter().print(expression))
+
+
+if __name__ == "__main__":
+    main()
