995bb26397a1b99159bc9ecea70a7f4a36d56fb3
Alex Clowes <alexclowes@gmail.com>
2021-09-23 23:42:31 +0100

    5.2 Implementing Syntax Trees

---
+++ tool/generate_ast.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+
+def define_type(file, base_name, class_name, field_list):
+    file.write(f"class {class_name}({base_name}):\n")
+    file.write(f"    def __init__(self, {field_list}):\n")
+    for field in field_list.split(","):
+        name = field.strip()
+        file.write(f"        self.{name} = {name}\n")
+    file.write("\n\n")
+
+
+def define_ast(output_dir, base_name, types):
+    path = os.path.join(output_dir, base_name.lower() + ".py")
+    with open(path, mode="w") as f:
+        f.write(f"class {base_name}:\n")
+        f.write("    pass\n\n\n")
+
+        # The AST classes
+        for type in types:
+            class_name, field_list = map(lambda s: s.strip(), type.split(":"))
+            define_type(f, base_name, class_name, field_list)
+
+
+def main():
+    args = sys.argv[1:]
+    if len(args) != 1:
+        print("Usage: generate_ast <output_directory>")
+        sys.exit(64)
+    output_dir = args[0]
+
+    define_ast(
+        output_dir,
+        "Expr",
+        [
+            "Binary : left, operator, right",
+            "Grouping : expression",
+            "Literal : value",
+            "Unary : operator, right",
+        ],
+    )
+
+
+if __name__ == "__main__":
+    main()
