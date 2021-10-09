d62c1904e8ccfb3c8920ae182a100d27092b01f8
Alex Clowes <alexclowes@gmail.com>
2021-10-09 22:48:21 +0100

    12.3 Creating Instances

--- plox/lox_class.py
+++ plox/lox_class.py
@@ -1,6 +1,23 @@
-class LoxClass:
+from .callable import Callable
+
+
+class LoxClass(Callable):
     def __init__(self, name):
         self.name = name
 
     def __str__(self):
         return self.name
+
+    def arity(self):
+        return 0
+
+    def call(self, interpreter, arguments):
+        return Instance(self)
+
+
+class Instance:
+    def __init__(self, class_):
+        self.class_ = class_
+
+    def __str__(self):
+        return f"{self.class_.name} instance"
